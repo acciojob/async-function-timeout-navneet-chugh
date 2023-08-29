6ab940e525c0b7eab92f419caea75f25b1f3462d
navneet-chugh <navneetchugh15@gmail.com>
2023-08-29 20:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+const button = document.getElementById("btn");
+const messageInput = document.getElementById("text");
+const delayInput = document.getElementById("delay");
+const output = document.getElementById("output");
+
+button.addEventListener("click", () => {
+  const message = messageInput.ariaValueMax;
+  const delay = Number(delayInput.value);
+
+  if (message && delay > 0) {
+    call(message, delay);
+  }
+});
+
+async function call(message, delay) {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, delay)
+  });
+
+output.innerText = message;
+}
